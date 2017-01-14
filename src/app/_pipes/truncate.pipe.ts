1892641de9ecfd6bdcbeab44830a740fe63b1e3b
Vedran Abramović <vabramovic@sciusa.com>
2017-01-14 22:58:23 +0100

    Created a pipe for truncating strings

--- src/app/_pipes/truncate.pipe.ts
+++ src/app/_pipes/truncate.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncatePipe implements PipeTransform {
 
   transform(value: string, args: string[]) : string {
-    let limit = args.length > 0 ? parseInt(args[0], 10) : 30;
-    let trail = args.length > 1 ? args[1] : '...';
+    let limit = 30;
+    let trail = '...';
 
     return value.length > limit ? value.substring(0, limit) + trail : value;
   }
